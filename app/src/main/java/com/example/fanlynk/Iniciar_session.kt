339c0c849df1a6d9bd7c0dfd4ddece0e3745b568
Timoty <luisangelrodriguez46@gmail.com>
2024-05-20 15:52:32 -0600

    Codigo hardcodeado

--- app/src/main/java/com/example/fanlynk/Iniciar_session.kt
+++ app/src/main/java/com/example/fanlynk/Iniciar_session.kt
@@ -1,7 +1,11 @@
 package com.example.fanlynk
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +21,24 @@ class Iniciar_session : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.botonIniciarSesion).setOnClickListener {
+            val correo_electronico = findViewById<EditText>(R.id.CorreoInicioSesion).text.toString()
+            val contraseña = findViewById<EditText>(R.id.contreaseñaInicioSesion).text.toString()
+
+            mostrarMensaje("Datos insertados correctamente")
+
+            val intent = Intent(this, Manejar_dispositivo::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<TextView>(R.id.textoInicioSesion).setOnClickListener {
+            val intent = Intent(this, registro::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun mostrarMensaje(mensaje: String) {
+        Toast.makeText(applicationContext, mensaje, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/fanlynk/registro.kt
+++ app/src/main/java/com/example/fanlynk/registro.kt
@@ -1,12 +1,17 @@
 package com.example.fanlynk
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class registro : AppCompatActivity() {
+class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,30 @@ class registro : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.btnRegistrarse).setOnClickListener {
+            val correo = findViewById<EditText>(R.id.textoCorreoRegistro).text.toString()
+            val usuario = findViewById<EditText>(R.id.usuarioRegistro).text.toString()
+            val contraseña = findViewById<EditText>(R.id.textContraseñaRegistro).text.toString()
+            val confContraseña = findViewById<EditText>(R.id.textConfContraseñaRegistro).text.toString()
+
+            if (contraseña == confContraseña) {
+                mostrarMensaje("Registrado")
+
+                val intent = Intent(this, Manejar_dispositivo::class.java)
+                startActivity(intent)
+            } else {
+                mostrarMensaje("Las contraseñas no coinciden")
+            }
+        }
+
+        findViewById<TextView>(R.id.textCuenta).setOnClickListener {
+            val intent = Intent(this, Iniciar_session::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun mostrarMensaje(mensaje: String) {
+        Toast.makeText(applicationContext, mensaje, Toast.LENGTH_SHORT).show()
     }
 }
