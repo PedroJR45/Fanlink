00bb799f4e966a46fd503a7af60cf1ee37e790c4
LaboratorioLTI2 <jrdz7974@gmail.com>
2024-05-24 11:25:29 -0600

    prueba vinculacion

--- app/src/main/java/com/example/fanlynk/vincular.kt
+++ app/src/main/java/com/example/fanlynk/vincular.kt
@@ -1,25 +1,20 @@
 package com.example.fanlynk
 
 
-import android.Manifest
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class vincular : AppCompatActivity() {
 
-    private lateinit var bluetoothAdapter: BluetoothAdapter
-    private lateinit var listDispo: ListView
+    private lateinit var pairedDevicesListView: ListView
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +25,32 @@ class vincular : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        pairedDevicesListView = findViewById(R.id.paired_devices_list)
+        showPairedDevices()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showPairedDevices() {
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        val deviceList = ArrayList<String>()
+
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceAddress = device.address
+            deviceList.add("$deviceName\n$deviceAddress")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceList)
+        pairedDevicesListView.adapter = adapter
+    }
+
+    private fun enableEdgeToEdge() {
+        // Implement your edge-to-edge method here if needed
     }
 }
+
+
 //        listDispo = findViewById(R.id.listDispo)
 //        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 //
