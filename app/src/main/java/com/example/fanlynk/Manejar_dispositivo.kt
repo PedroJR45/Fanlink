97c36e7b5d145433f69998f1e38edcb68c66bc5d
Timoty <luisangelrodriguez46@gmail.com>
2024-05-28 16:54:24 -0600

    Cambios Conectividad

--- app/src/main/java/com/example/fanlynk/Manejar_dispositivo.kt
+++ app/src/main/java/com/example/fanlynk/Manejar_dispositivo.kt
@@ -117,6 +117,20 @@ class Manejar_dispositivo : AppCompatActivity() {
             mmDevice = devices[0]
             try {
                 val mmUUID = "00001101-0000-1000-8000-00805F9B34FB"
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
                 mmSocket = mmDevice?.createInsecureRfcommSocketToServiceRecord(UUID.fromString(mmUUID))
                 mmSocket?.connect()
             } catch (e: Exception) {
