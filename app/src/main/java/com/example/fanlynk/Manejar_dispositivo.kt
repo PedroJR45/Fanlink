0fd70d1046144631ae99a7b03b560089d98faa11
Timoty <luisangelrodriguez46@gmail.com>
2024-05-28 13:25:15 -0600

    Cambios Conectividad

--- app/src/main/java/com/example/fanlynk/Manejar_dispositivo.kt
+++ app/src/main/java/com/example/fanlynk/Manejar_dispositivo.kt
@@ -1,32 +1,23 @@
 package com.example.fanlynk
 
-import android.Manifest
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.io.IOException
-import java.util.*
 
 class Manejar_dispositivo : AppCompatActivity() {
 
     private var bluetoothAdapter: BluetoothAdapter? = null
     private var bluetoothSocket: BluetoothSocket? = null
-    private val deviceAddress = "00:21:13:00:D7:2E"
-    private val uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB") // UUID para el HC-05
     private var lastSignal = "0"
-    private var isConnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,11 +44,7 @@ class Manejar_dispositivo : AppCompatActivity() {
         // Configuración del botón para enviar señal
         val buttonSendSignal = findViewById<Button>(R.id.button)
         buttonSendSignal.setOnClickListener {
-            if (!isConnected) {
-                connectToBluetoothDevice()
-            } else {
-                toggleAndSendBluetoothSignal()
-            }
+            toggleAndSendBluetoothSignal()
         }
 
         // Configuración del botón de logout
@@ -68,45 +55,11 @@ class Manejar_dispositivo : AppCompatActivity() {
 
         // Configuración del ImageView para enviar señal
         findViewById<ImageView>(R.id.imageEncendidoApagado).setOnClickListener {
-            if (!isConnected) {
-                connectToBluetoothDevice()
-            } else {
-                toggleAndSendBluetoothSignal()
-            }
-        }
-    }
-
-    private fun connectToBluetoothDevice() {
-        val device: BluetoothDevice? = bluetoothAdapter?.getRemoteDevice(deviceAddress)
-        try {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
-                    1
-                )
-                return
-            }
-            bluetoothSocket = device?.createRfcommSocketToServiceRecord(uuid)
-            bluetoothSocket?.connect()
-            isConnected = true
-            Toast.makeText(this, "Conectado", Toast.LENGTH_SHORT).show()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            isConnected = false
-            Toast.makeText(this, "Error al conectar", Toast.LENGTH_SHORT).show()
+            toggleAndSendBluetoothSignal()
         }
     }
 
     private fun toggleAndSendBluetoothSignal() {
-        if (!isConnected) {
-            Toast.makeText(this, "Dispositivo no conectado", Toast.LENGTH_SHORT).show()
-            return
-        }
         lastSignal = if (lastSignal == "0") "1" else "0"
         sendBluetoothSignal(lastSignal)
     }
